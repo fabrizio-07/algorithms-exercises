00f5fa0b0dee92ec91dc40b8cb17a52a656ec3cf
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-11-22 10:53:39 +0100

    feat(countNegatives.cpp): add first exercise of the array recursion ones.

---
+++ array/countNegatives.cpp
@@ -0,0 +1,73 @@
+/*
+Write a recursive function that counts the number of negatives numbers of the A array, which has n elements.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void printA(int A[], int n){
+
+    cout<<"A = { ";
+
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+
+    cout<<"}"<<endl;
+
+}
+
+unsigned short countNegatives(int A[], int n){
+
+    if(n==0)
+        return 0;
+
+    else if(A[n-1]>=0)
+        return countNegatives(A,n-1);
+
+    else
+        return 1 + countNegatives(A,n-1);
+
+}
+
+int main(int argc, char **argv){
+
+    int *A;
+    int n=0;
+
+    cout<<"This program counts the number of negative elements of an array."<<endl;
+
+    if(argc!=1){
+
+        n=argc-1;
+        A = new int[n];
+
+        for(int i=1;i<argc;i++)
+            A[i-1]=stoi(argv[i]);
+
+        printA(A,n);
+
+    }    
+
+    else{
+
+        cout<<"How many numbers do you want to insert in A?\t";
+        cin>>n;
+
+        A = new int[n];
+
+        for(int i=0;i<n;i++){
+            cout<<"Insert the "<<i+1<<"th number:\t";
+            cin>>A[i];
+        }
+
+        printA(A,n);
+
+    }
+
+    cout<<"There are "<<countNegatives(A,n)<<" negative numbers in A."<<endl;
+
+    delete[] A;
+    return 0;
+
+}
