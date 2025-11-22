ee2236f68ca098a8afea953b1652a0c4e30541a9
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-11-22 12:09:28 +0100

    feat(array): add second and third exercises.

---
+++ array/countNumAboveMean.cpp
@@ -0,0 +1,83 @@
+/*
+Write a recursive function that counts how many elements, of an array A of length n, have a value greater than the arithmetic mean of the array itself.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void printA(int A[], int n, float mean){
+
+    cout<<"A = { ";
+
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+
+    cout<<"}, mean = "<<mean<<endl;
+
+}
+
+unsigned long countNumAboveMean(int A[], int n, float mean){
+    
+    if(n==0)
+        return 0;
+
+    else if(A[n-1]>mean)
+        return 1 + countNumAboveMean(A,n-1,mean);
+
+    else
+        return countNumAboveMean(A,n-1,mean);
+
+}
+
+int main(int argc, char **argv){
+
+    int *A;
+    int n=0;
+    float mean=0;
+
+    cout<<"This program counts the number of elements of A array, which are greater than the arithmetic mean of the A."<<endl;
+
+    if(argc!=1){
+
+        n=argc-1;
+        A = new int[n];
+
+        for(int i=1;i<argc;i++)
+            A[i-1]=stoi(argv[i]);
+
+    }    
+
+    else{
+
+        cout<<"How many numbers do you want to insert in A?\t";
+        cin>>n;
+
+        while(n<=0){
+
+            cout<<"The length of A cannot be negative. How many numbers do you want to insert in A?\t";
+            cin>>n;
+
+        }
+
+        A = new int[n];
+
+        for(int i=0;i<n;i++){
+            cout<<"Insert the "<<i+1<<"th number:\t";
+            cin>>A[i];
+        }
+
+    }
+
+    for(int i=0;i<n;i++)
+        mean+=A[i];
+    mean=mean/n;
+
+    printA(A,n,mean);
+
+    cout<<"There are "<<countNumAboveMean(A,n,mean)<<" numbers in A greater than A mean, which is:\t"<<mean<<endl;
+
+    delete[] A;
+    return 0;
+
+}
---
+++ array/evenMultiply.cpp
@@ -0,0 +1,73 @@
+/*
+Write a recursive function that multiply all the even elements of the A array, with n numbers. If there are no even elements in A, the function must return 1.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void printA(int A[], int n){
+
+    cout<<"A = { ";
+
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+
+    cout<<"}"<<endl;
+
+}
+
+unsigned long evenMultiply(int A[], int n){
+
+    if(n==0)
+        return 1;
+
+    else if(A[n-1]%2!=0)
+        return evenMultiply(A,n-1);
+
+    else
+        return A[n-1] * evenMultiply(A,n-1);
+
+}   
+
+int main(int argc, char **argv){
+
+    int *A;
+    int n=0;
+
+    cout<<"This program multiplies the even elements of the A array, or returns 1 if there are no even elements in A."<<endl;
+
+    if(argc!=1){
+
+        n=argc-1;
+        A = new int[n];
+
+        for(int i=1;i<argc;i++)
+            A[i-1]=stoi(argv[i]);
+
+        printA(A,n);
+
+    }
+
+    else{
+    
+        cout<<"How many numbers do you want to insert in A?\t";
+        cin>>n;
+
+        A = new int[n];
+
+        for(int i=0;i<n;i++){
+            cout<<"Insert the "<<i+1<<"th element of A:\t";
+            cin>>A[i];
+        }
+
+        printA(A,n);
+
+    }
+
+    cout<<"The multiplication of the even numbers in A is:\t"<<evenMultiply(A,n)<<endl;
+
+    delete[] A;
+    return 0;
+
+}
