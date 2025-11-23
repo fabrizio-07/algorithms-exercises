672b6e6effe37ae5c39153dec19b4c9d2171d5d6
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-11-23 18:35:21 +0100

    feat(array/alternateMultiply.cpp): add the last exercise of array section.

---
+++ array/alternateMultiply.cpp
@@ -0,0 +1,90 @@
+/*
+Write a recursive function that calculates the alternated multiplication of the elements of the A array, mutliplying the first, dividing by the second, multiplying for the third and so on.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void printA(const int A[], int n){
+
+    cout<<"A = { ";
+
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+
+    cout<<"}"<<endl;
+
+}
+
+double alternateMultiply(const int A[], int n){
+
+    if(n==0)
+        return 1;
+
+    else if(n%2==0){
+    
+        if(A[n-1]==0){
+
+            cout<<"ERROR: division by zero."<<endl;
+            return 0;
+
+        }
+
+        return alternateMultiply(A,n-1)/A[n-1];
+    
+    }
+
+    else
+        return A[n-1] * alternateMultiply(A,n-1);
+
+}
+
+int main(int argc, char **argv){
+
+    int *A;
+    int n;
+
+    cout<<"This program prints the alternated multiplication of the elements of the A array."<<endl;
+
+    if(argc!=1){
+
+        n=argc-1;
+        A = new int[n];
+
+        for(int i=0;i<n;i++)
+            A[i]=stoi(argv[i+1]);
+
+    }
+
+    else{
+
+        cout<<"How many elements do you want to insert in A?\t";
+        cin>>n;
+
+        while(n<=0){
+
+            cout<<"A length must be positive. How many elements do you want to insert in A?\t";
+            cin>>n;
+
+        }
+
+        A = new int[n];
+
+        for(int i=0;i<n;i++){
+
+            cout<<"Insert the "<<i+1<<"th element:\t";
+            cin>>A[i];
+
+        }
+
+    }
+
+    printA(A,n);
+
+    cout<<"The alternated multiplication of the A array is equal to:\t"<<alternateMultiply(A,n)<<endl;
+
+    delete[] A;
+    return 0;
+
+}
