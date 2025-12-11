1caf3a10a8f96a9b700c62aee6b6be30ec9d0e7d
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-12-11 15:29:22 +0100

    feat(/lib/bst.h): update bst.h to work without templates.

--- lib/bst.h
+++ lib/bst.h
@@ -3,53 +3,50 @@
 
 #include <iostream>
 
-template <typename T>
 class Node{
 
 private:
-    T value;
-    Node<T> *left, *right, *parent;
+    int value;
+    Node *left, *right, *parent;
 
 public:
-    Node(T newValue=T()) : 
+    Node(int newValue=0) : 
         value(newValue), left(nullptr), right(nullptr), parent(nullptr){}
 
     //  getters
-    T getValue() const {return value;}
-    Node<T> * getLeft() const {return left;}
-    Node<T> * getRight() const {return right;}
-    Node<T> * getParent() const {return parent;}
+    int getValue() const {return value;}
+    Node *getLeft() const {return left;}
+    Node *getRight() const {return right;}
+    Node *getParent() const {return parent;}
 
     //  setters
-    void setValue(const T newValue) {value = newValue;}
-    void setLeft(Node<T> * newLeft) {left = newLeft;}
-    void setRight(Node<T> * newRight) {right = newRight;}
-    void setParent(Node<T> * newParent) {parent = newParent;}
+    void setValue(const int newValue) {value = newValue;}
+    void setLeft(Node *newLeft) {left = newLeft;}
+    void setRight(Node *newRight) {right = newRight;}
+    void setParent(Node *newParent) {parent = newParent;}
 
 };
 
-template <typename T>
 class BST{
 
 private:
-    Node<T> *root;
+    Node *root;
     
     //  copy constructor and copy assignment operator are private to prevent those operations
     BST(const BST&);
     BST& operator=(const BST&);
 
     //  helper function for destructor
-    void deleteSubtree(Node<T> *);
+    void deleteSubtree(Node *);
 
 public:
-    BST(Node<T> * newRoot=nullptr) : root(newRoot){std::cout<<"Creating BST..."<<std::endl;}
+    BST(Node *newRoot=nullptr) : root(newRoot){std::cout<<"Creating BST..."<<std::endl;}
     ~BST();
 
-    const Node<T> *getRoot() const {return root;}
+    const Node *getRoot() const {return root;}
 
-    void insert(const T);
+    void insert(const int);
 
 };
 
-#include "bst.tpp"
 #endif
