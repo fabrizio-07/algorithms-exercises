a8b1db1913f0cdf7b5f8f753f21fabe8cd76f4d7
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-12-15 23:53:45 +0100

    fix(/src/bst/getMin/getMin.h): avoid useless checks for the values of the nodes.

--- src/bst/getMin/getMin.h
+++ src/bst/getMin/getMin.h
@@ -13,11 +13,8 @@ int getMin(const Node *node, int min=INT_MAX){
     if(node==nullptr)
         return min;
 
-    else if(node->getValue()<min)
+    else 
         return getMin(node->getLeft(),node->getValue());
-
-    else
-        return getMin(node->getLeft(),min);
 
 }
 
